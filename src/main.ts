822b7009cf7206cd6913b44212e27ce6875fbece
lutitech <lutor4148@gmail.com>
2023-08-26 08:10:10 +0100

    cors origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'https://mailer-psi.vercel.app', // Replace with your frontend's URL
+    origin: '*', // Replace with your frontend's URL
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true, // If you're using cookies or other credentials
   });
